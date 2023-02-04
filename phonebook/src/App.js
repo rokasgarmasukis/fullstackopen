76d1d623b7fac798f0aba28730e439f59913ddd2
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-04 17:08:49 +0100

    Add notes app

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "15356587", id: 1 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const handleNameChange = (e) => setNewName(e.target.value);
+  const handleNumberChange = (e) => setNewNumber(e.target.value);
+
+  const addNumber = (e) => {
+    e.preventDefault();
+
+    let same;
+    persons.forEach((person) => {
+      if (person.name === newName) {
+        alert(`${person.name} is already added to phonebook`);
+        same = true;
+      }
+    });
+    if (same) return;
+
+    setPersons([
+      ...persons,
+      {
+        name: newName,
+        number: newNumber,
+        id: persons[persons.length - 1].id + 1,
+      },
+    ]);
+    setNewName("");
+    setNewNumber("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <p key={person.id}>
+            {person.name}: {person.number}
+          </p>
+        ))}
+      </div>
+
+      {/* <div>debug: {newName}</div> */}
     </div>
   );
-}
+};
 
 export default App;
