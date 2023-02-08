ae62bec08ea23e5001c995d128380b9276cb2b3b
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-08 10:00:52 +0100

    Change functionality to get notes from server

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import personsService from "./services/persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
@@ -18,7 +14,14 @@ const App = () => {
   const handleNumberChange = (e) => setNewNumber(e.target.value);
   const handleSearchChange = (e) => setNewSearch(e.target.value);
 
-  const addNumber = (e) => {
+  useEffect(() => {
+    personsService.getAll().then(persons => {
+      console.log("received");
+      setPersons(persons);
+    });
+  }, []);
+
+  const addPerson = (e) => {
     e.preventDefault();
 
     let same;
@@ -30,16 +33,17 @@ const App = () => {
     });
     if (same) return;
 
-    setPersons([
-      ...persons,
-      {
-        name: newName,
-        number: newNumber,
-        id: persons[persons.length - 1].id + 1,
-      },
-    ]);
-    setNewName("");
-    setNewNumber("");
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons[persons.length - 1].id + 1,
+    };
+
+    personsService.create(newPerson).then(person => {
+      setPersons([...persons, person]);
+      setNewName("");
+      setNewNumber("");
+    });
   };
 
   const personsToShow =
@@ -56,7 +60,7 @@ const App = () => {
 
       <h3>Add new:</h3>
       <PersonForm
-        onSubmit={addNumber}
+        onSubmit={addPerson}
         numberValue={newNumber}
         onNumberChange={handleNumberChange}
         nameValue={newName}
---
+++ phonebook/src/services/persons.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+  return request.then((res) => res.data);
+};
+
+const create = (newPerson) => {
+    const request = axios.post(baseUrl, newPerson)
+    return request.then(res => res.data)
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAll, create };
