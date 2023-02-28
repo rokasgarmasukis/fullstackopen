bb702e47abfc06a99556b2102388555d0c3f3257
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-28 18:47:08 +0100

    Add linting

--- backend/index.js
+++ backend/index.js
@@ -1,109 +1,109 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const Note = require("./models/note");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const Note = require('./models/note')
 // const cors = require("cors");
 
 // app.use(cors());
 
 const requestLogger = (request, response, next) => {
-  console.log("Method:", request.method);
-  console.log("Path:  ", request.path);
-  console.log("Body:  ", request.body);
-  console.log("---");
-  next();
-};
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
 
-app.use(express.static("build"));
-app.use(express.json());
-app.use(requestLogger);
+app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
 
-app.get("/api/notes", (req, res) => {
-  Note.find({}).then((notes) => {
-    res.json(notes);
-  });
-});
-
-app.get("/api/notes/:id", (req, res, next) => {
-  Note.findById(req.params.id)
-    .then((note) => {
-      if (note) {
-        res.json(note);
-      } else {
-        res.status(404).end();
-      }
+app.get('/api/notes', (req, res) => {
+    Note.find({}).then((notes) => {
+        res.json(notes)
     })
-    .catch((error) => next(error));
-});
+})
 
-app.post("/api/notes", (req, res, next) => {
-  const body = req.body;
+app.get('/api/notes/:id', (req, res, next) => {
+    Note.findById(req.params.id)
+        .then((note) => {
+            if (note) {
+                res.json(note)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch((error) => next(error))
+})
 
-  if (!body.content) {
-    return response.status(400).json({ error: "content missing" });
-  }
+app.post('/api/notes', (req, res, next) => {
+    const body = req.body
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-  });
+    if (!body.content) {
+        return res.status(400).json({ error: 'content missing' })
+    }
 
-  note
-    .save()
-    .then((savedNote) => {
-      res.json(savedNote);
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
     })
-    .catch((error) => next(error));
-});
 
-app.put("/api/notes/:id", (req, res, next) => {
-  const { content, important } = req.body;
+    note
+        .save()
+        .then((savedNote) => {
+            res.json(savedNote)
+        })
+        .catch((error) => next(error))
+})
 
-  Note.findByIdAndUpdate(
-    req.params.id,
-    { content, important },
-    { new: true, runValidators: true, context: "query" }
-  )
-    .then((updatedNote) => {
-      res.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
+app.put('/api/notes/:id', (req, res, next) => {
+    const { content, important } = req.body
 
-app.delete("/api/notes/:id", (req, res, next) => {
-  Note.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.status(204).end();
-    })
-    .catch((error) => next(error));
-});
+    Note.findByIdAndUpdate(
+        req.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: 'query' }
+    )
+        .then((updatedNote) => {
+            res.json(updatedNote)
+        })
+        .catch((error) => next(error))
+})
+
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch((error) => next(error))
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-  console.log("---");
+    console.error(error.message)
+    console.log('---')
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
-  next(error);
-};
+    next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    console.log(`Server running on port ${PORT}`)
+})
