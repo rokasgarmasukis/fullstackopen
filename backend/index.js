7610ff93bc43ab61421ac45f1b43ee6a5a59aafc
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-02-28 13:35:07 +0100

    Implement notes crud

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,16 @@ let notes = [
     content: "GET and POST are the most important methods of HTTP protocol",
     important: true,
   },
+  {
+    id: 4,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
 ];
+
+app.use(express.json())
+
+
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -26,6 +35,51 @@ app.get("/", (request, response) => {
 app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = +req.params.id
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    res.json(note)
+  } else {
+    res.status(404).end()
+  }
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(n => n.id))
+  : 0
+  return maxId + 1
+}
+
+app.post("/api/notes", (req, res) => {
+
+  const body = req.body
+
+  if (!body) {
+    return response.status(400).json({error: "content missing"})
+  }
+
+  const note = {
+    id: generateId(),
+    content: body.content,
+    important: body.important || false,
+  }
+
+  notes = notes.concat(note)
+
+  res.json(note)
+})
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = +req.params.id
+
+  notes = notes.filter(note => note.id != id)
+
+  res.status(204).end()
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
